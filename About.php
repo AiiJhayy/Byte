9b8b6de5b3f34575ab90f55a9de99250cb40dd9b
AJ <angeloredruco456@gmail.com>
2023-05-20 22:17:55 +0800

    new folder

---
+++ About.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "veiwport" content = "width=device-width, initial-scale = 1">
+    <title>About | ByteMe Co.</title>
+</head>
+
+<style> 
+h1 {
+    text-align: center;
+}
+</style>
+
+<body>
+
+
+<br>
+
+<div class = "page">
+    <h1>ABOUT PAGESaasasasas</h1>
+</div>
+
+</body>
+</html>
